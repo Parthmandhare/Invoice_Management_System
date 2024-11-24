227aee6df59cfb0c0df2857d4be93e51c416ea97
Parth Mandhare <parthmandhare@12gmail.com>
2024-11-24 05:58:24 +0530

    Commit by Parth

--- app/page.js
+++ app/page.js
@@ -1,40 +1,22 @@
-
-
-
-
-export default async function Home() {
-  
-  const data = [
-    {
-      vendor: "Volopay Pvt. Ltd.",
-      invoice: "No: 1000101 IN000000161",
-      status: "Awaiting Approval",
-      amount: "2,93,000",
-      date: "18-03-2024",
-      due: "28-04-2024",
-      department: "Marketing",
-      costCenter: "Bangalore",
-    },
-    // Add more rows as needed
-  ];
-
-  const getStatusBadge = (status) => {
-    const statusColors = {
-      "Awaiting Approval": "bg-purple-100 text-purple-700",
-      Open: "bg-blue-100 text-blue-700",
-      Approved: "bg-green-100 text-green-700",
-      "Vendor not found": "bg-orange-100 text-orange-700",
-      Processing: "bg-yellow-100 text-yellow-700",
-      Paid: "bg-teal-100 text-teal-700",
-    };
-  }  
+"use client"
+import { useRouter } from "next/navigation";
+export default function Home() {
+  const router = useRouter();
 
   return (
-   <>
-      
-      GO to /invoices please
-     
-      
-   </>
+    <>
+      <div className="w-full flex flex-col align-middle justify-center text-center mt-10 items-center gap-5">
+        <p className="text-xl font-semibold">
+          GO to <span className="text-2xl font-bold">/invoices</span> please{" "}
+        </p>
+        <p>OR</p>
+        <button
+          onClick={() => router.push("/invoices")}
+          className="border-2 shadow-lg bg-slate-400 py-2 px-2 rounded-md w-fit"
+        >
+          Go to Dashboard
+        </button>
+      </div>
+    </>
   );
 }
